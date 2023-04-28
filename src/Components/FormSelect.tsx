ac6b017078743ffb8544e0cb0b77e229f2cabe44
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-28 11:10:30 -0500

    handle change in FormSelect

--- src/Components/FormSelect.tsx
+++ src/Components/FormSelect.tsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
+
 interface Option {
-    value: string;
-    label: string;
-  }
-  
+  value: string;
+  label: string;
+}
+
 export interface FormSelectProps {
   id: string;
   label?: string;
   options: Option[];
   selected?: string;
   inputClass?: string;
+  onChange?: (value: string) => void;
 }
 
-export function FormSelect({ id, label, options, selected, inputClass }: FormSelectProps): JSX.Element {
+export function FormSelect({
+  id,
+  label,
+  options,
+  selected,
+  inputClass,
+  onChange,
+}: FormSelectProps): JSX.Element {
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    selected
+  );
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    setSelectedValue(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
   return (
     <div className={inputClass}>
       <label htmlFor={id} className="form-label">
         {label}
       </label>
-      <select className="form-select" name="" id={id} defaultValue={selected}>
+      <select
+        className="form-select"
+        name=""
+        id={id}
+        value={selectedValue}
+        onChange={handleOnChange}
+      >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
@@ -27,3 +55,4 @@ export function FormSelect({ id, label, options, selected, inputClass }: FormSel
     </div>
   );
 }
+
--- src/inputs/Formacion.tsx
+++ src/inputs/Formacion.tsx
@@ -1,6 +1,14 @@
-import {FormSelect} from "../Components/FormSelect";
+import {FormSelect, FormSelectProps} from "../Components/FormSelect";
+import { useState } from "react";
 
 function Formacion() {
+
+  const [gradoAcademico, setGradoAcademico] = useState<string | undefined>('0');
+
+  const handleGradoAcademicoChange = (value: string) => {
+    setGradoAcademico(value);
+  };
+  
 
   const options = [
     { value: "0", label: "Ninguno" },
@@ -17,7 +25,8 @@ function Formacion() {
           id="grado" 
           label="A. Grado académico (registrado en SUNEDU o MINEDU)" 
           options={options} 
-          selected="0"
+          selected={gradoAcademico}
+          onChange={handleGradoAcademicoChange}
         />
       </div>
     );
