24fb7169ca57804e06eec38f941a9b48e299af08
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-28 01:59:27 -0500

    display better the degrees

--- src/inputs/Asesoria.tsx
+++ src/inputs/Asesoria.tsx
@@ -4,23 +4,24 @@ function Asesoria() {
     return ( 
         <div>
             <h6>F. Haber asesorado o co-asesorado tesis sustentadas y aprobadas</h6>
+            <p>Para obtención del grado de:</p>
             <FormNumber 
                 id="asesoria-doctor"
-                label="Para obtención del grado de Doctor"
+                label="Doctor"
                 defaultValue={0}
                 minValue={0}
                 inputClass="mb-3"
                 />
             <FormNumber 
                 id="asesoria-magister"
-                label="Para obtención del grado de Magister"
+                label="Magister"
                 defaultValue={0}
                 minValue={0}
                 inputClass="mb-3"
                 />
             <FormNumber 
                 id="asesoria-titulo"
-                label="Para obtención del grado de Bachiller o Título Profesional"
+                label="Bachiller o Título Profesional"
                 defaultValue={0}
                 minValue={0}
                 inputClass="mb-3"
